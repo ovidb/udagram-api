e4d18b13313bf9c04ee7706f832f9aedc898ce66
ovidb <antarexis@gmail.com>
2020-06-22 04:44:38 +0100

    Add travis and docker

--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -1,5 +1,4 @@
 import {Router, Request, Response} from 'express';
-import {FeedRouter} from './feed/routes/feed.router';
 import {UserRouter} from './users/routes/user.router';
 
 const router: Router = Router();
